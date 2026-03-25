2d27312917e3d9646d2603c672956edf24e9cd97
Kozak Tatiana <om.nom.omg.t@gmail.com>
2026-03-25 21:58:53 +0200

    new hm3

--- src/s2-homeworks/hw03/Greeting.tsx
+++ src/s2-homeworks/hw03/Greeting.tsx
@@ -9,7 +9,7 @@ type GreetingPropsType = {
     onEnter: (e:KeyboardEvent<HTMLInputElement>) => void // need to fix any
     error: string // need to fix any
     totalUsers: number // need to fix any
-    lastUserName?: any // need to fix any
+    lastUserName?: string // need to fix any
 }
 
 // презентационная компонента (для верстальщика)
@@ -25,7 +25,7 @@ const Greeting: React.FC<GreetingPropsType> = (
         lastUserName,
     } // деструктуризация пропсов
 ) => {
-    const inputClass = s.errorInput // need to fix with (?:)
+    const inputClass = error ? s.errorInput : s.input// need to fix with (?:)
 
     return (
 
