123aab2e99877ef6a39584002fe37538f316396e
Kozak Tatiana <om.nom.omg.t@gmail.com>
2026-05-13 22:53:04 +0300

    hm4

--- src/s2-homeworks/hw04/Stand.tsx
+++ src/s2-homeworks/hw04/Stand.tsx
@@ -8,7 +8,7 @@ const Stand = () => {
     const [stateForAllInputs, setValue] = useState<string>('')
     const [error, setError] = useState<string>('')
 
-    const [stateForAllCheckboxes, setChecked] = useState<boolean>(false)
+    const [stateForAllCheckboxes, setChecked] = useState<boolean>(true)
 
     return (
         <div id={'hw4-stand'} className={s.stand}>
