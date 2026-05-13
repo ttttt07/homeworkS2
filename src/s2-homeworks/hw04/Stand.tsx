cb610f82802104547e22482411c3dfcf203e2ce2
Kozak Tatiana <om.nom.omg.t@gmail.com>
2026-05-13 23:11:25 +0300

    hm43

--- src/s2-homeworks/hw04/Stand.tsx
+++ src/s2-homeworks/hw04/Stand.tsx
@@ -8,8 +8,8 @@ const Stand = () => {
     const [stateForAllInputs, setValue] = useState<string>('')
     const [error, setError] = useState<string>('')
 
-    const [stateForAllCheckboxes, setChecked] = useState<boolean>(true)
-
+    const [stateForAllCheckboxes, setChecked] = useState<boolean>(false)
+    console.log(stateForAllCheckboxes)
     return (
         <div id={'hw4-stand'} className={s.stand}>
             <div className={s.inputs}>
@@ -91,7 +91,7 @@ const Stand = () => {
                     <SuperCheckbox
                         id={'hw4-super-checkbox-like-old'}
                         checked={stateForAllCheckboxes}
-                        onChange={(e) => setChecked(e.currentTarget.checked)}
+                       // onChange={(e) => setChecked(e.currentTarget.checked)}
                     />
                 </div>
             </div>
